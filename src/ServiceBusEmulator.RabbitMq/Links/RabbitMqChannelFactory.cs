0f731e781ff82b4c00aa6570fe70c3684c2a419d
Piotr Rojek <dra9ula@gmail.com>
2023-02-12 21:51:47 +0100

    add health check to code and docker

---
+++ src/ServiceBusEmulator.RabbitMq/Links/RabbitMqChannelFactory.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+using ServiceBusEmulator.RabbitMq.Options;
+
+namespace ServiceBusEmulator.RabbitMq.Links
+{
+    public class RabbitMqChannelFactory : IRabbitMqChannelFactory, IDisposable
+    {
+        private readonly RabbitMqBackendOptions _options;
+
+        private IConnection? _connection;
+        private ConnectionFactory _connectionFactory;
+
+        private bool _disposed;
+
+        public RabbitMqChannelFactory(IOptions<RabbitMqBackendOptions> options)
+        {
+            _options = options.Value;
+
+            _connectionFactory = new()
+            {
+                Password = _options.Password,
+                UserName = _options.Username,
+                HostName = _options.Host,
+                Port = _options.Port
+            };
+        }
+
+        protected IConnection Connection
+        {
+            get
+            {
+                if (_disposed)
+                {
+                    throw new ObjectDisposedException(nameof(RabbitMqChannelFactory));
+                }
+
+                if (!(_connection?.IsOpen ?? false))
+                {
+                    lock (_connectionFactory)
+                    {
+                        if (!(_connection?.IsOpen ?? false))
+                        {
+                            _connection = _connectionFactory.CreateConnection();
+                        }
+                    }
+                }
+
+                return _connection;
+            }
+        }
+
+        public IModel CreateChannel()
+        {
+            return Connection.CreateModel();
+        }
+
+        public void Dispose()
+        {
+            if (!_disposed && _connection != null)
+            {
+                _connection.Dispose();
+                _connection = null;
+            }
+            _disposed = true;
+        }
+    }
+}
---
+++ src/ServiceBusEmulator.RabbitMq/RabbitMqHealthCheck.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using ServiceBusEmulator.RabbitMq.Links;
+
+namespace ServiceBusEmulator.RabbitMq
+{
+    public class RabbitMqHealthCheck : IHealthCheck
+    {
+        private readonly IRabbitMqChannelFactory _channelFactory;
+
+        public RabbitMqHealthCheck(IRabbitMqChannelFactory channelFactory)
+        {
+            _channelFactory = channelFactory;
+        }
+
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                using var model = _channelFactory.CreateChannel();
+                return Task.FromResult(HealthCheckResult.Healthy());
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(new HealthCheckResult(context.Registration.FailureStatus, exception: ex));
+            }
+        }
+    }
+}
